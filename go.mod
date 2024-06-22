41cb37e5e13af6f61cff51dff1da81115924d6fa
fluke <jesadakorn.k@finstable.co.th>
2024-06-22 15:10:09 +0700

    deploy test

--- go.mod
+++ go.mod
@@ -10,7 +10,7 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
-	github.com/bytedance/sonic v1.11.8 // indirect
+	github.com/bytedance/sonic v1.11.9 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
@@ -25,18 +25,18 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
-	github.com/golang/snappy v0.0.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/joho/godotenv v1.5.1
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/swaggo/swag v1.16.3
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
@@ -44,7 +44,7 @@ require (
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
-	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	github.com/youmark/pkcs8 v0.0.0-20240424034433-3c2c7870ae76 // indirect
 	go.mongodb.org/mongo-driver v1.15.1
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
--- main.go
+++ main.go
@@ -1,183 +1,207 @@
+// package main
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// 	"strconv"
+// 	"time"
+
+// 	"github.com/gin-contrib/cors"
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/joho/godotenv"
+// 	swaggerFiles "github.com/swaggo/files"
+// 	ginSwagger "github.com/swaggo/gin-swagger"
+// 	"go.mongodb.org/mongo-driver/bson"
+// 	"go.mongodb.org/mongo-driver/bson/primitive"
+// 	"go.mongodb.org/mongo-driver/mongo"
+// 	"go.mongodb.org/mongo-driver/mongo/options"
+// )
+
+// var client *mongo.Client
+
+// type Document struct {
+// 	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+// 	Symbol   string             `bson:"Symbol" json:"Symbol"`
+// 	Year     string             `bson:"Year" json:"Year"`
+// 	Quarter  string             `bson:"Quarter" json:"Quarter"`
+// 	Datetime primitive.DateTime `bson:"Datetime" json:"Datetime"`
+// 	Url      string             `bson:"Url" json:"Url"`
+// 	EPS      float64            `bson:"EPS" json:"EPS"`
+// }
+
+// func main() {
+// 	port := os.Getenv("PORT")
+// 	if port == "" {
+// 		port = "8080"
+// 	}
+
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
+
+// 	mongoURI := os.Getenv("MONGO_URI")
+// 	if mongoURI == "" {
+// 		log.Fatal("MONGO_URI environment variable not set")
+// 	}
+
+// 	// client, err = mongo.NewClient(options.Client().ApplyURI(mongoURI))
+// 	client, err = mongo.NewClient(options.Client().ApplyURI(mongoURI))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 	defer cancel()
+
+// 	err = client.Connect(ctx)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	err = client.Ping(ctx, nil)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	fmt.Println("Connected to MongoDB!")
+
+// 	r := gin.Default()
+// 	r.Use(cors.Default())
+// 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+// 	api := r.Group("/api")
+// 	{
+// 		api.GET("/hello", helloHandler)
+// 		api.GET("/data", getDataHandler)
+// 		api.GET("/symbols", getUniqueSymbolsHandler)
+// 	}
+
+// 	// if err := r.Run(":" + port); err != nil {
+// 	// 	log.Fatal(err)
+// 	// }
+// 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+// }
+
+// func helloHandler(c *gin.Context) {
+// 	c.JSON(http.StatusOK, gin.H{
+// 		"message": "Hello, World!",
+// 	})
+// }
+
+// func getDataHandler(c *gin.Context) {
+// 	collection := client.Database("StockThaiAnalysis").Collection("processed")
+
+// 	limit, err := strconv.Atoi(c.DefaultQuery("limit", "10"))
+// 	if err != nil || limit <= 0 {
+// 		limit = 10
+// 	}
+// 	page, err := strconv.Atoi(c.DefaultQuery("page", "1"))
+// 	if err != nil || page <= 0 {
+// 		page = 1
+// 	}
+// 	skip := (page - 1) * limit
+
+// 	sort := c.DefaultQuery("sort", "_id")
+// 	order := c.DefaultQuery("order", "asc")
+// 	sortOrder := 1
+// 	if order == "desc" {
+// 		sortOrder = -1
+// 	}
+
+// 	filter := bson.D{}
+// 	if symbol := c.Query("Symbol"); symbol != "" {
+// 		filter = append(filter, bson.E{Key: "Symbol", Value: symbol})
+// 	}
+
+// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+// 	defer cancel()
+
+// 	var results []Document
+
+// 	opts := options.Find().SetLimit(int64(limit)).SetSkip(int64(skip)).SetSort(bson.D{{Key: sort, Value: sortOrder}})
+// 	cursor, err := collection.Find(ctx, filter, opts)
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+// 		return
+// 	}
+// 	defer cursor.Close(ctx)
+
+// 	for cursor.Next(ctx) {
+// 		var doc Document
+// 		err := cursor.Decode(&doc)
+// 		if err != nil {
+// 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+// 			return
+// 		}
+// 		results = append(results, doc)
+// 	}
+
+// 	if err := cursor.Err(); err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+// 		return
+// 	}
+
+// 	total, err := collection.CountDocuments(ctx, filter)
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+// 		return
+// 	}
+
+// 	c.JSON(http.StatusOK, gin.H{
+// 		"data":  results,
+// 		"total": total,
+// 		"page":  page,
+// 		"limit": limit,
+// 	})
+// }
+
+// func getUniqueSymbolsHandler(c *gin.Context) {
+// 	collection := client.Database("StockThaiAnalysis").Collection("processed")
+
+// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+// 	defer cancel()
+
+// 	query := c.Query("query")
+
+// 	filter := bson.M{}
+// 	if query != "" {
+// 		filter = bson.M{"Symbol": bson.M{"$regex": query, "$options": "i"}}
+// 	}
+
+// 	symbols, err := collection.Distinct(ctx, "Symbol", filter)
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+// 		return
+// 	}
+
+// 	c.JSON(http.StatusOK, gin.H{
+// 		"symbols": symbols,
+// 	})
+// }
+
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
-	"time"
-
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var client *mongo.Client
-
-type Document struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Symbol   string             `bson:"Symbol" json:"Symbol"`
-	Year     string             `bson:"Year" json:"Year"`
-	Quarter  string             `bson:"Quarter" json:"Quarter"`
-	Datetime primitive.DateTime `bson:"Datetime" json:"Datetime"`
-	Url      string             `bson:"Url" json:"Url"`
-	EPS      float64            `bson:"EPS" json:"EPS"`
-}
-
 func main() {
+	http.HandleFunc("/", handler)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	mongoURI := os.Getenv("MONGO_URI")
-	if mongoURI == "" {
-		log.Fatal("MONGO_URI environment variable not set")
-	}
-
-	client, err = mongo.NewClient(options.Client().ApplyURI(mongoURI))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Connected to MongoDB!")
-
-	r := gin.Default()
-	r.Use(cors.Default())
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
-	api := r.Group("/api")
-	{
-		api.GET("/hello", helloHandler)
-		api.GET("/data", getDataHandler)
-		api.GET("/symbols", getUniqueSymbolsHandler)
-	}
-
-	// if err := r.Run(":" + port); err != nil {
-	// 	log.Fatal(err)
-	// }
+	log.Printf("Listening on port %s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
-func helloHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello, World!",
-	})
-}
-
-func getDataHandler(c *gin.Context) {
-	collection := client.Database("StockThaiAnalysis").Collection("processed")
-
-	limit, err := strconv.Atoi(c.DefaultQuery("limit", "10"))
-	if err != nil || limit <= 0 {
-		limit = 10
-	}
-	page, err := strconv.Atoi(c.DefaultQuery("page", "1"))
-	if err != nil || page <= 0 {
-		page = 1
-	}
-	skip := (page - 1) * limit
-
-	sort := c.DefaultQuery("sort", "_id")
-	order := c.DefaultQuery("order", "asc")
-	sortOrder := 1
-	if order == "desc" {
-		sortOrder = -1
-	}
-
-	filter := bson.D{}
-	if symbol := c.Query("Symbol"); symbol != "" {
-		filter = append(filter, bson.E{Key: "Symbol", Value: symbol})
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	var results []Document
-
-	opts := options.Find().SetLimit(int64(limit)).SetSkip(int64(skip)).SetSort(bson.D{{Key: sort, Value: sortOrder}})
-	cursor, err := collection.Find(ctx, filter, opts)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-	defer cursor.Close(ctx)
-
-	for cursor.Next(ctx) {
-		var doc Document
-		err := cursor.Decode(&doc)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
-		results = append(results, doc)
-	}
-
-	if err := cursor.Err(); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	total, err := collection.CountDocuments(ctx, filter)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"data":  results,
-		"total": total,
-		"page":  page,
-		"limit": limit,
-	})
-}
-
-func getUniqueSymbolsHandler(c *gin.Context) {
-	collection := client.Database("StockThaiAnalysis").Collection("processed")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	query := c.Query("query")
-
-	filter := bson.M{}
-	if query != "" {
-		filter = bson.M{"Symbol": bson.M{"$regex": query, "$options": "i"}}
-	}
-
-	symbols, err := collection.Distinct(ctx, "Symbol", filter)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"symbols": symbols,
-	})
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
 }
